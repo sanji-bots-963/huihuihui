3655abbfd7a7995e209682e5d2102406ee1135d9
sanji-bots-963 <uwvohrc155@nezahuys.xyz>
2024-07-26 08:31:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7255245444:AAF4m0w3SKjAyC4d7boUZRaJUE1g3JJD_D4")
-API_ID = int(os.environ.get("API_ID", "24567281"))
-API_HASH = os.environ.get("API_HASH", "17a51c14e6c25caad1a8f63a97c51f96")
-OWNER = int(os.environ.get("OWNER", "6727550037"))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "File_Renamer_VFast_Bot")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID = int(os.environ.get("API_ID", ""))
+API_HASH = os.environ.get("API_HASH", "")
+OWNER = int(os.environ.get("OWNER", ""))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
 
 FORCE_SUBS = os.environ.get("FORCE_SUBS", "Straw_Hat_Bots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031978775"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
 
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://sagatobots00001:sagatobots100@cluster00001.vgdshkw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster00001")
-DB_NAME = os.environ.get("DB_NAME", "zoro_4gb_file")
+DB_NAME = os.environ.get("DB_NAME", "")
 
 STRING = os.environ.get("STRING", "")
 BOT_PIC = os.environ.get("BOT_PIC", "https://telegra.ph//file/a9a177e24269cd69cb4fe.jpg")
